947114905bf0d64589e04e5ea592298cdb2cd229
amh6281 <amh6281@naver.com>
2022-01-26 14:32:06 +0900

    버튼 이벤트 구현, Todo의 변수 타입 정의 2022.01.26

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
+import { idText } from "typescript";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <span className="heading">ToDo-List</span>
-      <InputFeild todo={todo} setTodo={setTodo} />
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
